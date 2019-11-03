709353db00e807e684c897ed74007ef4408718af
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2019-11-02 23:10:54 -0400

    Made adjustments to be more responsive

--- src/app/sermons-summary/sermons-summary.component.ts
+++ src/app/sermons-summary/sermons-summary.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SermonsSummaryComponent implements OnInit {
 
+  sermonsLink = '/sermons';
+
   constructor() { }
 
   ngOnInit() {
