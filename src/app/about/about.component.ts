8a6135049a7e528918928f0381783f4f18056b0d
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2019-11-28 21:03:23 -0500

    Changed image extension. Removed deacon stuff.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,9 +12,7 @@ export class AboutComponent implements OnInit {
 
   readonly staffURL = '../assets/about-json/staff.json';
   staffImages: StaffImage[];
-  deaconImages: StaffImage[];
   staffTitle = 'Meet the Staff';
-  deaconTItle = 'Meet our Deacons';
 
   constructor(private data: DataService) { }
 
@@ -22,7 +20,6 @@ export class AboutComponent implements OnInit {
     this.data.getData(this.staffURL).pipe(take(1))
       .subscribe((results: StaffArray) => {
         this.staffImages = results.staffArray;
-        this.deaconImages = results.deaconArray;
       });
   }
 
