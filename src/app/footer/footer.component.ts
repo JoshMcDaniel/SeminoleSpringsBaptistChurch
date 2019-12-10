63d9cd6216f49a6ff42c70417cae4325583af211
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2019-12-09 22:17:00 -0500

    Added GitHub link to developer text in footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -9,10 +9,18 @@ export class FooterComponent implements OnInit {
 
   readonly COPYWRITE = '2019 Seminole Springs Baptist Church';
   readonly DEVELOPER = 'Website developed by Josh McDaniel';
+  readonly DEVELOPER_LINK = 'https://github.com/JoshMcDaniel';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /**
+   * Opens a new window with the developers webpage.
+   */
+  developerPage(): void {
+    window.open(this.DEVELOPER_LINK, '_blank');
   }
 
 }
